e4c0954c28d9090e10c4556411455604f4a48d71
JorgeAntonio512 <jorge.antonio.de.segundo@gmail.com>
2015-02-03 18:14:05 -0600

    trying to integrate Community Engine

---
+++ communityengine-master/commandsapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Commandsapp::Application.config.secret_key_base = 'e00b854dc5d42c4dd56354d86c1cf110dbeaf18a565f25c09620bc97fb5b43ad51a155a6f75e1aa3f5dcfef4a85ca874aeac5543f9d9c144db61eda65049166f'
